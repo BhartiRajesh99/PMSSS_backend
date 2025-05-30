0ca952e4872d50f24c0652e3219d5eec1b80c65b
RAJ <raku81453@gmail.com>
2025-05-30 23:35:55 +0530

    update error response class

--- controllers/authController.js
+++ controllers/authController.js
@@ -237,7 +237,7 @@ export const loginStudent = async (req, res, next) => {
     console.log("Student lookup result:", student ? "Found" : "Not found");
 
     if (!student) {
-      return res.json(
+      return next(
         new ErrorResponse(
           "No student account found with this email. Please register first.",
           404
