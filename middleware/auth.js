44b177eda8adc76a572015a86f32d833737ab481
RAJ <raku81453@gmail.com>
2025-05-31 13:43:34 +0530

    finance dashboard

--- middleware/auth.js
+++ middleware/auth.js
@@ -55,9 +55,9 @@ export const protect = async (req, res, next) => {
 };
 
 // Grant access to specific roles
-export const authorize = (...roles) => {
+export const authorize = (roles) => {
   return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
+    if (roles !== req.user.role) {
       return next(
         new ErrorResponse(
           `User role ${req.user.role} is not authorized to access this route`,
